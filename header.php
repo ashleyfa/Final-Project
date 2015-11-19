5f352bb944077f4fc83d6b2e6f97f8d7b027003c
David Do <david@dodavid.com>
2015-11-18 19:00:42 -1000

    Stripped out the header navigation bar
    Pulled the Navbar and put it into a header.php for scalability.  Now we
    just edit the header.php to have the Navbar reflect on all pages
    without editing each page.

---
+++ header.php
@@ -0,0 +1,27 @@
+<header>
+        <div class ="navbarHead">
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">Logo Here</a>
+                    </div>
+                    <div class="collapse navbar-collapse" id="myNavbar">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="/home/">Home</a></li>
+                            <li><a href="/shop">Shop</a></li>
+                            <li><a href="/about">About Us</a></li>
+                            <li><a href="/contact">Contact</a></li>
+
+                            <li><a href="/signup/"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                            <li><a href="/signup/"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
--- index.php
+++ index.php
@@ -15,33 +15,9 @@
     <script src="js/script.js"></script>
 </head>
 <body>
-    <header>
-        <div class ="navbarHead">
-            <nav class="navbar navbar-default">
-                <div class="container-fluid">
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="#">Logo Here</a>
-                    </div>
-                    <div class="collapse navbar-collapse" id="myNavbar">
-                        <ul class="nav navbar-nav navbar-right">
-                            <li><a href="/home/">Home</a></li>
-                            <li><a href="/shop">Shop</a></li>
-                            <li><a href="/about">About Us</a></li>
-                            <li><a href="/contact">Contact</a></li>
-
-                            <li><a href="/signup/"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                            <li><a href="/signup/"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
-    </header>
+    <?php 
+    include_once('header.php');
+    ?>
     <section id="main">
         <div class="container-fluid">
             <div clas="mainWrapper">
