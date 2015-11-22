2c7f8e597e09b9ed05cb3a5f3ca88c369ea76ce1
David Do <david@dodavid.com>
2015-11-21 22:52:19 -1000

    Registration
    Created the registration page with php validation and javascript
    password validation check.

--- test/index.php
+++ test/index.php
@@ -4,84 +4,31 @@
 <style>
 .error {color: #FF0000;}
 </style>
+<script type="text/javascript" src="passtest.js"></script>
 </head>
 <body> 
 
-<?php
-// define variables and set to empty values
-$nameErr = $emailErr = $genderErr = $websiteErr = "";
-$name = $email = $gender = $comment = $website = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-   if (empty($_POST["name"])) {
-     $nameErr = "Name is required";
-   } else {
-     $name = test_input($_POST["name"]);
-     // check if name only contains letters and whitespace
-     if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-       $nameErr = "Only letters and white space allowed"; 
-     }
-   }
-   
-   if (empty($_POST["email"])) {
-     $emailErr = "Email is required";
-   } else {
-     $email = test_input($_POST["email"]);
-     // check if e-mail address is well-formed
-     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-       $emailErr = "Invalid email format"; 
-     }
-   }
-     
-   if (empty($_POST["website"])) {
-     $website = "";
-   } else {
-     $website = test_input($_POST["website"]);
-     // check if URL address syntax is valid
-     if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
-       $websiteErr = "Invalid URL"; 
-     } 
-   }
-
-   if (empty($_POST["comment"])) {
-     $comment = "";
-   } else {
-     $comment = test_input($_POST["comment"]);
-   }
-
-   if (empty($_POST["gender"])) {
-     $genderErr = "Gender is required";
-   } else {
-     $gender = test_input($_POST["gender"]);
-   }
-}
-
-function test_input($data) {
-   $data = trim($data);
-   $data = stripslashes($data);
-   $data = htmlspecialchars($data);
-   return $data;
-}
+<?php 
+require 'validation.php';
 ?>
 
 <h2>PHP Form Validation Example</h2>
 <p><span class="error">* required field.</span></p>
-<form method="post" action="connect.php"> 
-   Name: <input type="text" name="name">
-   <span class="error">* <?php echo $nameErr;?></span>
+<form method="post" action="index.php"> 
+   First Name: <input type="text" name="fname">
+   <span class="error">* <?php echo $fnameErr;?></span>
+   <br><br>
+   Last Name: <input type="text" name="lname">
+   <span class="error">* <?php echo $lnameErr;?></span>
    <br><br>
    E-mail: <input type="text" name="email">
    <span class="error">* <?php echo $emailErr;?></span>
    <br><br>
-   Website: <input type="text" name="website">
-   <span class="error"><?php echo $websiteErr;?></span>
+   Password: <input type="text" id="pass1" name="password">
+   <span class="error">* <?php echo $passwordErr;?></span>
    <br><br>
-   Comment: <textarea name="comment" rows="5" cols="40"></textarea>
-   <br><br>
-   Gender:
-   <input type="radio" name="gender" value="female">Female
-   <input type="radio" name="gender" value="male">Male
-   <span class="error">* <?php echo $genderErr;?></span>
+   Re-enter Password: <input type="text" id="pass2" name="repassword" onkeyup="checkPass(); return false;">
+   <span id="confirmMessage"class="error">* <?php echo $repasswordErr;?></span>
    <br><br>
    <input type="submit" name="submit" value="Submit"> 
 </form>
